c2a695dfedc1a22784ed9c1818f059aa64f8e6ad
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2025-06-28 19:03:22 -0300

    Update BaseInteraction.cs

--- Assets/_Game/_Scripts/Interaction/BaseInteraction.cs
+++ Assets/_Game/_Scripts/Interaction/BaseInteraction.cs
@@ -1,22 +1,8 @@
-using UnityEngine;
-using UnityEngine.Events;
-
 namespace Game.Interaction
 {
     public interface IInteractable
     {
         bool IsInteractable { get; }
         void Interact();
-    }
-
-    public abstract class BaseInteraction : MonoBehaviour, IInteractable
-    {
-        public virtual bool IsInteractable => true;
-        public UnityEvent OnInteract;
-
-        public virtual void Interact()
-        {
-            OnInteract?.Invoke();
-        }
     }
 }
