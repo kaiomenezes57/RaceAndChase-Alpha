6b5e88bf5dbed6a24f818b532e136e6b01e9aae7
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2025-06-28 15:15:38 -0300

    Update EntityComponents.cs

--- Assets/_Game/_Scripts/Entity/Components/EntityComponents.cs
+++ Assets/_Game/_Scripts/Entity/Components/EntityComponents.cs
@@ -1,4 +1,8 @@
+using Game.Events;
+using Game.Interaction;
+using Game.Utils.Collision;
 using Sirenix.OdinInspector;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -30,6 +34,7 @@ namespace Game.Entity.Components
     {
         [SerializeField] private CharacterController _characterController;
         [SerializeField, MinValue(0f)] private float _movementSpeed;
+        [SerializeField, MinValue(0f)] private float _rotationSpeed;
         [SerializeField] private InputActionReference _moveAxis;
 
         public bool IsActive { get; set; }
@@ -43,6 +48,76 @@ namespace Game.Entity.Components
             _motion.z = input.y;
 
             _characterController.Move(_movementSpeed * Time.deltaTime * _motion);
+
+            if (IsMoving)
+            {
+                owner.transform.rotation = Quaternion.Slerp(
+                    owner.transform.rotation,
+                    Quaternion.LookRotation(_motion),
+                    _rotationSpeed * Time.deltaTime
+                );
+            }
+        }
+    }
+
+    public sealed class InteractionBrowser_EntityComponent : IEntityComponentInitializer, 
+        IEntityComponentUpdater,
+        IEntityComponentCleaner
+    {
+        public bool IsActive { get; set; }
+        private IInteractable _currentInteractable;
+        
+        [SerializeField] private Raycast_CustomCollision _raycast;
+        [SerializeField] private InputActionReference _interactInput;
+
+        private IInteractable CurrentInteractable
+        {
+            get => _currentInteractable;
+            set
+            {
+                if (_currentInteractable == value) return;
+                _currentInteractable = value;
+
+                EventBus.Raise(new InteractionFound_GameEvent(CurrentInteractable));
+            }
+        }
+
+        public void Initialize(MonoBehaviour owner)
+        {
+            _interactInput.action.Enable();
+            _interactInput.action.performed += TryInteract;
+        }
+
+        public void Clear(MonoBehaviour owner)
+        {
+            _interactInput.action.Disable();
+            _interactInput.action.performed -= TryInteract;
+        }
+
+        public void Update(MonoBehaviour owner)
+        {
+            if (_raycast.GetCollisions(transform: owner.transform, amount: 1) is { Length: > 0 } collisions && 
+                collisions.First() is { } firstCollision)
+            {
+                if (firstCollision.TryGetComponent<IInteractable>(out var interactable) &&
+                    interactable.IsInteractable)
+                {
+                    CurrentInteractable = interactable;
+                    return;
+                }
+            }
+
+            CurrentInteractable = null;
+        }
+
+        private void TryInteract(InputAction.CallbackContext _)
+        {
+            if (CurrentInteractable is not { IsInteractable: true }) return;
+            
+            CurrentInteractable.Interact();
+            
+            EventBus.Raise(new InteractionStart_GameEvent(CurrentInteractable));
+            CurrentInteractable = null;
         }
     }
 }
